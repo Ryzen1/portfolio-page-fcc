4dc053fbe92a09c680582ed9ab4e1cea002d4c7e
Yahtzee <abdullahalbahou1@gmail.com>
2017-02-19 09:14:26 +0300

    Finished the project

--- jquery.js
+++ jquery.js
@@ -1,11 +1,32 @@
-$(document).ready(function()
-{
-    var headerHeight = $("nav").outerHeight();
+$(document).ready(function () {
+
+    $("#contact-form-message").focus(function () {
+        $("#contact-form-message").val("");
+    });
+
+    $("#contact-form-message").focusout(function () {
+        $("#contact-form-message").val("Message");
+    });
+
+    $("input").focus(function () {
+        this.value = "";
+    });
+
+    $("input").focusout(function () {
+            if (this.id === "contact-form-name") 
+                this.value = "Name"
+            if (this.id === "contact-form-email")
+                this.value = "Email Address";
+            if (this.id === "contact-form-number")
+                this.value = "Phone Number";
+    });
     
-    $(".slide-section").click(function(e){
-        
+    var headerHeight = $("nav").outerHeight();
+
+    $(".slide-section").click(function (e) {
+
         var linkHref = $(this).attr('href');
-        
+
         $("html, body").animate({
             scrollTop: $(linkHref).offset().top - headerHeight
         }, 1000);
